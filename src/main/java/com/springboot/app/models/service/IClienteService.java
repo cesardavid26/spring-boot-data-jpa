b166b829dab3d14280f7643bbeda617d53c6bf53
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-02-19 20:02:17 -0500

    metodo para buscar productos en el service

--- src/main/java/com/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/springboot/app/models/service/IClienteService.java
@@ -19,4 +19,6 @@ public interface IClienteService {
 	public void delete(Long id);
 	public List<Producto> findByNombre(String term);
 	public void saveFactura(Factura factura);
+	
+	public Producto findProductoById(Long id);
 }
