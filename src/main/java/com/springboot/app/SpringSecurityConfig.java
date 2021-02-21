9e5e5cf6f7970aedf3cb6eb998d9f17f1a3d473c
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-02-21 15:25:56 -0500

    creando la clase LoginController

--- src/main/java/com/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/springboot/app/SpringSecurityConfig.java
@@ -29,7 +29,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers("/factura/**").hasAnyRole("ADMIN")
 		.anyRequest().authenticated()
 		.and()
-		.formLogin().permitAll()
+		   .formLogin().loginPage("/login")
+		   .permitAll()
 		.and()
 		.logout().permitAll();
 	}
