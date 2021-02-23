f35c7f921dcb894d2ad2f84f47d6fa402b7dc38a
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-02-23 12:19:03 -0500

    obteniendo programaticamente el role del usuario en el controlador

--- src/main/java/com/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/springboot/app/controllers/ClienteController.java
@@ -2,12 +2,13 @@
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collection;
 import java.util.Map;
 
 import javax.validation.Valid;
 
-import org.apache.juli.logging.Log;
-import org.apache.juli.logging.LogFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -16,6 +17,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -93,6 +97,12 @@ public class ClienteController {
 		
 		if(auth!=null) {
 			logger.info("Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): Usuario autenticado, tu username es: ".concat(authentication.getName()));
+		}
+		
+		if(hasRole("ROLE_ADMIN")) {
+			logger.info("Hola ".concat(auth.getName()).concat(" tienes acceso!"));
+		}else {
+			logger.info("Hola ".concat(auth.getName()).concat(" NO tienes acceso!"));
 		}
 		
 		Pageable pageRequest = PageRequest.of(page, 5);
@@ -193,4 +203,31 @@ public class ClienteController {
 
 		return "redirect:/listar";
 	}
-}
+	
+	private boolean hasRole(String role) {
+		
+		SecurityContext context = SecurityContextHolder.getContext();
+		
+		if(context==null) {
+			return false;
+		}
+		Authentication auth = context.getAuthentication();
+		
+		if(auth==null) {
+		return false;
+		}
+		
+		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+		return authorities.contains(new SimpleGrantedAuthority(role));
+		/*
+		for(GrantedAuthority authority: authorities) {
+			if(role.equals(authority.getAuthority())) {
+				logger.info("Hola usuario ".concat(auth.getName()).concat(" tu rol es ".concat(authority.getAuthority())));
+				return true;
+			}
+		}
+		return false;*/
+		}
+		
+	}
+
